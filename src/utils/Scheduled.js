b1d9f67114cf146275ef0f89b60a943b91d1f258
shirin <yuda.pratama37@gmail.com>
2021-10-19 12:21:13 +0700

    [FIX] fix unnecesary logging

--- src/utils/Scheduled.js
+++ src/utils/Scheduled.js
@@ -7,19 +7,18 @@ const { DateTime } = require('luxon');
 
 module.exports = class Scheduled {
 
-    dailyRotation(client){
-        cron.schedule('0 3 * * *', () => {
-            const logch = process.env.DEPLOY == 'DEV' ? client.channels.cache.get(config.devlog) : client.channels.cache.get(config.log)
-            const today = DateTime.now().setZone('Asia/Jakarta').weekday - 1
-            const rotatEmbed = new botEmbed()
-            .domainRotation(today)
-            .setImage(rotationData[today].image)
-            logch.send({embeds: [rotatEmbed]});
-            console.log('aaaaa');
-        },{
-            timezone: "Asia/Jakarta"
-        })
+	dailyRotation(client) {
+		cron.schedule('0 3 * * *', () => {
+			const logch = process.env.DEPLOY == 'DEV' ? client.channels.cache.get(config.devlog) : client.channels.cache.get(config.log);
+			const today = DateTime.now().setZone('Asia/Jakarta').weekday - 1;
+			const rotatEmbed = new botEmbed()
+				.domainRotation(today)
+				.setImage(rotationData[today].image);
+			logch.send({embeds: [rotatEmbed]});
+		}, {
+			timezone: "Asia/Jakarta"
+		});
 
-    }
+	}
 
-}
+};
