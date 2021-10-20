a214c000cd16a2e996a056027f82717816289216
shirin <yuda.pratama37@gmail.com>
2021-10-20 13:09:22 +0700

    [REFACTOR] changed channel storage to database

--- src/utils/Scheduled.js
+++ src/utils/Scheduled.js
@@ -1,20 +1,33 @@
 const cron = require('node-cron');
-const {MessageAttachment} = require('discord.js');
 const config = require('../../config.json');
 const { rotationData } = require('../assets/data/ObjectCollection');
 const botEmbed = require('./EmbedBuilder');
 const { DateTime } = require('luxon');
+const MDiscord = require('../models/guild');
+const {Op} = require('sequelize');
 
 module.exports = class Scheduled {
 
-	dailyRotation(client) {
-		cron.schedule('0 3 * * *', () => {
-			const logch = process.env.DEPLOY == 'DEV' ? client.channels.cache.get(config.devlog) : client.channels.cache.get(config.log);
+	async dailyRotation(client) {
+		cron.schedule('21 10 * * *', async () => {
+			const getLogCh = await MDiscord.findAll({
+				attributes: ['daily_channel'],
+				where: {
+					daily_channel:{
+						[Op.not]: null
+					}
+				}
+			});
+			const logch = getLogCh.map((e) => e.daily_channel);
 			const today = DateTime.now().setZone('Asia/Jakarta').weekday - 1;
 			const rotatEmbed = new botEmbed()
 				.domainRotation(today)
 				.setImage(rotationData[today].image);
-			logch.send({embeds: [rotatEmbed]});
+			logch.forEach(e => {
+				const getch = client.channels.cache.get(e);
+				getch.send({embeds: [rotatEmbed]});
+			});
+
 		}, {
 			timezone: "Asia/Jakarta"
 		});
